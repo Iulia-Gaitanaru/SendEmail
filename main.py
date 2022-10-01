ff871a312fa0e038b6d43f3271962ecb4e4e8264
Gaitanaru Iulia <77321172+Iulia-Gaitanaru@users.noreply.github.com>
2022-10-01 15:29:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+# Customize email to each specific person
+if __name__ == '__main__':
+    html = Template(Path('index.html').read_text())
+    email = EmailMessage()
+    email['from'] = 'Gaitanaru Iulia'
+    # TODO: write the email of the person to whom you are sending mail
+    email['to'] = ''
+    email['subject'] = 'You won 1,000,000 $$$'
+    email.set_content(html.substitute(name='dummy'), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    # TODO: write your email and password
+    smtp.login('', '')
+    smtp.send_message(email)
+    print("Sent it!")
